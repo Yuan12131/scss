0ddd52df06e2954678f1a9a75dd16c8ca23a0fe1
leeyuan <dbdks32123@gmail.com>
2023-10-10 13:08:35 +0900

    writeFile data 수정

--- app.js
+++ app.js
@@ -7,16 +7,13 @@ let htmlli = `<li>${data[0]}</li>`
 let pokemonLi=[];
 
 for (i = 0; i < data.length; i++){
-  for (j=pokemonLi.length; j <= pokemonLi.length; j++){
+  for (j = pokemonLi.length; j <= pokemonLi.length; j++){
   pokemonLi[j] = "<li>" + data[i] + "</li>"
   break
-  
   }
 }
   console.log(pokemonLi)
   
-    fs.writeFile('pokemon.html', '<html><head></head><body><ul>' + pokemonLi.toString() + '</ul></body></html>', function() {
+    fs.writeFile('pokemon.html', '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><ul>' + pokemonLi.toString() + '</ul></body></html>', function() {
       console.log('File created');
     });
-
-
