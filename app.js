8fc648b68440881980c8842a2548a56c633360ad
leeyuan <dbdks32123@gmail.com>
2023-10-10 13:02:57 +0900

    불러온 npm 패키지의 배열을 html을 생성해서 li태그 내용으로 삽입 -> 쉼표 삭제해야함

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
 const pokemon = require('pokemon');
-console.log(pokemon.all());
+const data = pokemon.all('ko')
+let htmlli = `<li>${data[0]}</li>`
+let pokemonLi=[];
+
+for (i = 0; i < data.length; i++){
+  for (j=pokemonLi.length; j <= pokemonLi.length; j++){
+  pokemonLi[j] = "<li>" + data[i] + "</li>"
+  break
+  
+  }
+}
+  console.log(pokemonLi)
+  
+    fs.writeFile('pokemon.html', '<html><head></head><body><ul>' + pokemonLi.toString() + '</ul></body></html>', function() {
+      console.log('File created');
+    });
+
+
